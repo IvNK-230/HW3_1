88029daf9c3d72b3b005705c4468261e6425e49b
Ivan Kashuba <kashubaim@gmail.com>
2023-06-28 21:03:24 +0600

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,13 @@
+public class BonusMilesService {
+    public int calculate(int price){
+        int miles;
+
+        if (price >= 1000) {
+            miles = price / 20;
+        }
+        else{
+            miles = 0;
+        }
+        return miles;
+    }
+}
